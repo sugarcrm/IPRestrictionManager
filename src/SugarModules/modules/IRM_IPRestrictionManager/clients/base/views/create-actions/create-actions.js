f06233aa03e6658c37bb7d1978db0ce13955f41a
Jerry Clark <geraldbclark@gmail.com>
2017-04-14 11:04:19 -0400

    Update create-actions.js

--- src/SugarModules/modules/IRM_IPRestrictionManager/clients/base/views/create-actions/create-actions.js
+++ src/SugarModules/modules/IRM_IPRestrictionManager/clients/base/views/create-actions/create-actions.js
@@ -2,15 +2,12 @@
     extendsFrom: 'CreateView',
 
     initialize: function (options) {
-        if (!app.acl.hasAccess('view', 'Administration'))
-        {
+        if (!app.acl.hasAccess('view', 'Administration')) {
             app.controller.loadView({
                 layout: 'access-denied'
             });
-        }
-        else
-        {
-            app.view.invokeParent(this, {type: 'view', name: 'create', method: 'initialize', args: [options]});
+        } else {
+            this._super('initialize', [options]);
         }
     },
 
